6604fba4ddca34e3c6a11724eb912e341a8d5e7f
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 11:36:59 -0400

    Modif Exe2

--- exercice2.sh
+++ exercice2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#Étape 1. Afficher un menu
 
-#Étape 2. Mettre le menu dans une fonction
+#Étape 1. Afficher un menu et le mettre le menu dans une fonction
 
 afficherMenu(){
     echo "Veuillez choisir une option"
@@ -11,7 +10,7 @@ afficherMenu(){
     read choix
 }
 
-#Étape 3. Faire une boucle:
+#Étape 2. Faire une boucle:
     #- offrir 2 choix: Quitter (choix q),
     #- ou appeler ./exercice1.sh (choix d)
 #À chaque début de boucle, afficher le menu
