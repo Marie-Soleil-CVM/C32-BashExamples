35a207b637205a014aba148669c9326fe20a1ce1
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 12:21:49 -0400

    Ajout exe3

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+afficherEspace(){
+ for ((j = 0;j < $1;j++))
+    do
+        echo -n " "        
+    done
+}
+
+
+for ((i = 0;i < 10;i++))
+do
+    clear
+    afficherEspace $i
+    if [[ $i -lt 9 ]]
+    then
+        echo -n "@"        
+    else
+        echo " BOOM"
+    fi
+        sleep 0.2
+done
+echo ""
+
