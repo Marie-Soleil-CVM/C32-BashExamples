ea8e85af7d35022c4a035ced1360ac53409b66e5
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 10:44:13 -0400

    Ajout notes boucle

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+#*****************************************************************************************Boucle for**************************************************
+
+for ((i = 0;i < 5;i++))
+do
+    echo $i
+done
+
+#*************************************************Ranger le résultat d'une commande dans une variable avec ``*****************************************
+
+liste=`ls`     #`..` pour que la commande ls soit exécutée. On peut mettre le résultat de la commande dans une variable.
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+
+#*****************************************************************************************Boucle while**************************************************
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
