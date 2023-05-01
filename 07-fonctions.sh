765cdd6a15fb253a3afa4c1910a748c879ed7f15
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 10:56:01 -0400

    Ajout notes fonction

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+maFonction(){
+    echo "paramètres: " $1          # $1 va renvoyer le premier paramètre, $2 va renvoyer le 2ième paramètre etc. (comme dans les notes de variables)
+}
+
+maFonction "salut"         # pas de () pour appeler la fonction avec un paramètre (juste un espace)
+maFonction "toi"
