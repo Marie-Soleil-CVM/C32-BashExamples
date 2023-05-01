5ce3eed27d5957d5c47cf2d679609902520fd448
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 11:03:33 -0400

    Ajout notes fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+
+if test -e $1             # -e veut dire **fichier** existe. Avec le test vs les [[]], on peut ajouter des -e ou autre
+then
+    echo "le fichier existe"
+fi
+
+
+if test -f $1             # -f veut dire c'est un fichier. Avec le test vs les [[]], on peut ajouter des -e ou autre
+then
+    echo "le fichier existe"
+fi
+
+
+if test -d $1             # -d veut dire c'est un dossier. Avec le test vs les [[]], on peut ajouter des -e ou autre
+then
+    echo "le fichier existe"
+fi
