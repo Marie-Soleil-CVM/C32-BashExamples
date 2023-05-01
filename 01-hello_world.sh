25d27e8c8edbb83330aa84cdc5a6590037cd89d7
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 09:47:17 -0400

    Ajout notes dans hello_world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "hello" $USER
+
+# exit pour quitter le programme
+
+echo -n "Vive "  # -n permet de ne pas sauter a la prochaine ligne
+echo "Linux"
+
+echo -e "1\n2\n3" # -e permet d'interpreter les retours de ligne et etc (\n, \t, ..)
+
+read -p "Quel age avez-vous ? " age 
+echo $age " Vous etes jeune !"
+
+# -p pour demander une question.
+# si on veut afficher une variable, on met $ dedans, sinon pas de $ devant
