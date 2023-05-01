c8319c4295489e548e79ae1f188a15bad263b4fa
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 10:03:42 -0400

    Ajout notes calcul

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo 10+10
+echo $((10+10))   #pour afficher un résultat de calcul, on doit mettre $(())
+valeur=$((10+10))  # on peut aussi ranger le résultat dans une variable puis l'afficher
+echo $valeur
+
+
+read -p "nb 1: " nb1
+read -p "nb 2: " nb2
+let resultat=$nb1*$nb2   #avec let, pas besoin des (())
+echo $resultat
