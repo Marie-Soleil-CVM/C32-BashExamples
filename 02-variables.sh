0af6c6634f51a365a08d2817eb0e9ad2f7a8b140
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 09:56:56 -0400

    Ajout notes sur variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+prenom=Neo  # pas d'espace quand on effecte une variable !!! Pas de $ parce quLon affecte vs on n'affiche pas
+echo $prenom
+
+declare -i age=33   # -i pour déclarer un integer (pour forcer la type). Pas obligatoire
+
+declare -r chemin=/bin    # -r pour déclarer une constante
+
+echo "Nom script: " $0   # afficher le nom du script
+echo "Nombre de paramètres: " $#  #afficher le nb de paramètre
+echo "perso: " $1  # range le premier paramètre dans la variable perso
+
