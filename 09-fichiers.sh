d47dff708fdced4f4e3613171dc62b0ab1e2206d
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-01 12:51:57 -0400

    Rien

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -9,11 +9,11 @@ fi
 
 if test -f $1             # -f veut dire c'est un fichier. Avec le test vs les [[]], on peut ajouter des -e ou autre
 then
-    echo "le fichier existe"
+    echo "c'est un fichier"
 fi
 
 
 if test -d $1             # -d veut dire c'est un dossier. Avec le test vs les [[]], on peut ajouter des -e ou autre
 then
-    echo "le fichier existe"
+    echo "c'est un dossier"
 fi
