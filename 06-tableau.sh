3145c07c29b2bc6e3dce415650d204a875498d68
Marie-Soleil-CVM <sunny_davis@hotmail.fr>
2023-05-02 12:38:20 -0400

    Correction tableau

--- 06-tableau.sh
+++ 06-tableau.sh
@@ -4,6 +4,6 @@
 
 declare -a film=(The matrix)    # -a pour déclarer un tableau
 
-echo $(film[0])
-echo $(film[1])
-echo "Nombre d'éléments: " $(film[@])
+echo ${film[0]}
+echo ${film[1]}
+echo "Nombre d'éléments: " ${film[@]}
